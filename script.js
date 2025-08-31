95a21e63166c528c14542bf6e18179463066367f
Riyan081 <waterdlaw810@gmail.com>
2025-08-31 13:26:24 +0530

    hero sec don e

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-var t1 = gsap.timeline();
+document.addEventListener("mousemove",function(dets){
+   gsap.to("#cursor",{
+    left:dets.x,
+    top:dets.y,
+})
+})
+
+
+
+function lodingAnimation(){
+    var t1 = gsap.timeline();
 
 t1.from(".line h1", {
   y: 150,
@@ -19,7 +29,7 @@ t1.from("#line1-part1", {
         grow++;
         h5timer.innerHTML = grow;
       }
-    }, 25);
+    }, 1);
   },
 });
 
@@ -32,18 +42,40 @@ t1.to(" .line h2",{
 t1.to("#loader", {
   opacity: 0,
   stagger: 0.3,
-  
-  delay: 3,
+  delay: 0.1,
 });
 
 t1.from("#page1",{
     y:1200,
     opactigy:0,
     delaty:0.2,
-   duration:0.5
+   duration:0.6
 })
 
 t1.to("#loader",{
     display:"none"
 }
 )
+
+t1.from("#nav",{
+  opacity:0,
+})
+
+t1.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2,
+})
+}
+
+lodingAnimation();
+
+
+
+Shery.makeMagnet("#nav-part2 p" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+
+
